e99f559f97c8a286d2dc8e42b82882adcea271bd
Jair Sanchez Futema <LC211061193@mulege.tecnm.mx>
2021-11-24 00:35:25 -0700

    Se agrego el archivo conexion a la base de datos

---
+++ conexion2.php
@@ -0,0 +1,12 @@
+<?php
+	//// Conexion a la BD //// 
+	$servidor = "localhost";
+	$usuario = "itesmeit_itesmei";
+	$clave="^5zE]hcvfLRc";
+	$base = "itesmeit_JairFutema"; // OJO aquí va el nombre de su BD 
+	$conexion = new mysqli($servidor,$usuario,$clave,$base);
+	if($conexion -> connect_errno)
+	{
+		die("Fallo la conexión :(".$conexion -> mysqli_connect_errno().")".$conexion->mysqli_connect_error());
+	}
+?>
