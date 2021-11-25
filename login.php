9db5cb61bba1c6862503b69ced8be35072b10bae
Jesus Manuel Monroy Quintero <l181061128@mulege.tecnm.mx>
2021-11-24 21:22:57 -0700

    Se creó un archivo login funcional con instrucciones

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+$usuario_correcto = "Jair";
+$palabra_secreta_correcta = "2312";
+$usuario = $_POST["usuario"];
+$palabra_secreta = $_POST["palabra_secreta"];
+
+if ($usuario === $usuario_correcto && $palabra_secreta === $palabra_secreta_correcta) {
+ 
+    session_start();
+    $_SESSION["usuario"] = $usuario;
+    header("Location: \index.html"); #<--Aqui debe de ir el archivo que se va a mostrar en caso de que la contraseña este bien#
+ 
+} else {
+    echo "El usuario o la contraseña son incorrectos";
+}
